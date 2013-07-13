52e915e631f685cd9e49c453b288228925e6e8a9
avitebskiy <aleksey.vitebskiy@gmail.com>
2013-07-13 10:22:56 -0400

    Added the full implementation of PyMan.

---
+++ 2013.07.13/pygame/PyMan/PyMan.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python
+
+import os, sys
+import pygame
+from pygame.locals import *
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', 'images')
+    fullname = os.path.join(fullname, name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+class PyManMain:
+    """The Main PyMan Class - This class handles the main
+    initialization and creating of the Game."""
+
+    def __init__(self, width=640,height=480):
+        """Initialize"""
+        """Initialize PyGame"""
+        pygame.init()
+        """Set the window Size"""
+        self.width = width
+        self.height = height
+        """Create the Screen"""
+        self.screen = pygame.display.set_mode((self.width
+                                               , self.height))
+
+    def MainLoop(self):
+        """This is the Main Loop of the Game"""
+
+        """Load All of our Sprites"""
+        self.LoadSprites();
+        """tell pygame to keep sending up keystrokes when they are
+        held down"""
+        pygame.key.set_repeat(500, 30)
+
+        """Create the background"""
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((0,0,0))
+
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if ((event.key == K_RIGHT)
+                    or (event.key == K_LEFT)
+                    or (event.key == K_UP)
+                    or (event.key == K_DOWN)):
+                        self.snake.move(event.key)
+
+            """Check for collision"""
+            lstCols = pygame.sprite.spritecollide(self.snake
+                                                 , self.pellet_sprites
+                                                 , True)
+            """Update the amount of pellets eaten"""
+            self.snake.pellets = self.snake.pellets + len(lstCols)
+
+            """Do the Drawging"""
+            self.screen.blit(self.background, (0, 0))
+            if pygame.font:
+                font = pygame.font.Font(None, 36)
+                text = font.render("Pellets %s" % self.snake.pellets
+                                    , 1, (255, 0, 0))
+                textpos = text.get_rect(centerx=self.background.get_width()/2)
+                self.screen.blit(text, textpos)
+
+            self.pellet_sprites.draw(self.screen)
+            self.snake_sprites.draw(self.screen)
+            pygame.display.flip()
+
+    def LoadSprites(self):
+        """Load the sprites that we need"""
+        self.snake = Snake()
+        self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
+
+        """figure out how many pellets we can display"""
+        nNumHorizontal = int(self.width/64)
+        nNumVertical = int(self.height/64)
+        """Create the Pellet group"""
+        self.pellet_sprites = pygame.sprite.Group()
+        """Create all of the pellets and add them to the
+        pellet_sprites group"""
+        for x in range(nNumHorizontal):
+            for y in range(nNumVertical):
+                self.pellet_sprites.add(Pellet(pygame.Rect(x*64, y*64, 64, 64)))
+
+class Snake(pygame.sprite.Sprite):
+    """This is our snake that will move around the screen"""
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('snake.png',-1)
+        self.pellets = 0
+        """Set the number of Pixels to move each time"""
+        self.x_dist = 5
+        self.y_dist = 5
+
+    def move(self, key):
+        """Move your self in one of the 4 directions according to key"""
+        """Key is the pyGame define for either up,down,left, or right key
+        we will adjust outselfs in that direction"""
+        xMove = 0;
+        yMove = 0;
+
+        if (key == K_RIGHT):
+            xMove = self.x_dist
+        elif (key == K_LEFT):
+            xMove = -self.x_dist
+        elif (key == K_UP):
+            yMove = -self.y_dist
+        elif (key == K_DOWN):
+            yMove = self.y_dist
+        #self.rect = self.rect.move(xMove,yMove);
+        self.rect.move_ip(xMove,yMove);
+
+class Pellet(pygame.sprite.Sprite):
+
+    def __init__(self, rect=None):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('pellet.png',-1)
+        if rect != None:
+            self.rect = rect
+
+
+if __name__ == "__main__":
+    MainWindow = PyManMain()
+    MainWindow.MainLoop()
